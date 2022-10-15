0783dc62c93c4f2c37e063ba72d589db84c5a5b4
Amila Ruwan Guruge <amilaruwan9349@gmail.com>
2022-10-15 18:09:28 +0200

    Cleaned and added Dto spaces

---
+++ D4APIClient/Dtos/D4SpaceDto.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace D4APIClient.Dtos
+{
+    public partial class D4SpaceDto
+    {
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+
+    public partial class Data
+    {
+        [JsonPropertyName("spaces")]
+        public Spaces Spaces { get; set; }
+    }
+
+    public partial class Node
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("points")]
+        public Spaces Points { get; set; }
+    }
+
+    public partial class Edge
+    {
+        [JsonPropertyName("node")]
+        public Node Node { get; set; }
+    }
+
+    public partial class Spaces
+    {
+        [JsonPropertyName("edges")]
+        public Edge[] Edges { get; set; }
+    }
+}
